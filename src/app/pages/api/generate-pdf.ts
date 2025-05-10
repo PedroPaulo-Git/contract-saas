464ac46b3fc10d2b5f703f5ee01bfd08bc7f103f
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-05-10 13:07:31 -0300

    generate-pdf.ts commented

--- src/app/pages/api/generate-pdf.ts
+++ src/app/pages/api/generate-pdf.ts
@@ -1,14 +1,14 @@
-import { generatePdf } from "../../../../lib/pdf";
-import { NextRequest, NextResponse } from "next/server";
+// import { generatePdf } from "../../../../lib/pdf";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const pdfBuffer = await generatePdf(body);
+// export async function POST(req: NextRequest) {
+//   const body = await req.json();
+//   const pdfBuffer = await generatePdf(body);
 
-  return new NextResponse(pdfBuffer, {
-    headers: {
-      "Content-Type": "application/pdf",
-      "Content-Disposition": 'attachment; filename="document.pdf"',
-    },
-  });
-}
+//   return new NextResponse(pdfBuffer, {
+//     headers: {
+//       "Content-Type": "application/pdf",
+//       "Content-Disposition": 'attachment; filename="document.pdf"',
+//     },
+//   });
+// }
