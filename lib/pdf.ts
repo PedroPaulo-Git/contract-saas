38e1cc7ef9f3d9a9b7a24a069ca009286a67900e
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-05-10 12:56:31 -0300

    COMMENT PDF.TS

--- lib/pdf.ts
+++ lib/pdf.ts
@@ -17,18 +17,18 @@
 
 //   return Buffer.from(pdfUint8Array); // 🔧 conversão correta aqui
 // }
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 
-export async function generatePdf(data: any): Promise<Buffer> {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+// export async function generatePdf(data: any): Promise<Buffer> {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
 
-  await page.setContent(`<html><body>${data.text}</body></html>`, {
-    waitUntil: 'networkidle0',
-  });
+//   await page.setContent(`<html><body>${data.text}</body></html>`, {
+//     waitUntil: 'networkidle0',
+//   });
 
-  const pdfUint8Array = await page.pdf({ format: 'A4' });
- await browser.close();
+//   const pdfUint8Array = await page.pdf({ format: 'A4' });
+//  await browser.close();
 
-return Buffer.from(pdfUint8Array); 
-}
+// return Buffer.from(pdfUint8Array); 
+// }
