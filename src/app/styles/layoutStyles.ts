7ed4969981a11f70716687e64d73caafdc77c5cd
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-05-13 02:31:53 -0300

    breaking lines NDA layout

--- src/app/styles/layoutStyles.ts
+++ src/app/styles/layoutStyles.ts
@@ -2,22 +2,30 @@ import { PdfSettings } from "../../../components/PdfConfig";
 
 const getGlobalStyles = (isPDF: boolean, config: PdfSettings): string => {
   return `
-
+* {
+  box-sizing: border-box;
+}
     .pdf {
       color: black;
-      padding: ${isPDF ? "0.5cm" : "1.5cm"};
+     padding: ${isPDF ? "0.5cm" : "1.5cm"};
     }
  
-  p, h1, h2, h3, .paragraph {
+ .paragraph {
   page-break-inside: avoid;
 }
-p, h1, h2, h3 {
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
+  p, h1, h2, h3, h4, h5, h6, .pdf, section {
+    page-break-inside: avoid;
+    break-inside: avoid;
+    orphans: 3;
+    widows: 3;
+  }
+    .section {
+  page-break-inside: avoid;
 }
-
-
+.page-break {
+    page-break-before: always;
+    break-before: always;
+  }
     h1 { font-size: ${isPDF ? config.h1Size : "1rem"}; text-align: center; ${config.underlineH1 ? "text-decoration: underline;" : ""}}
     h2 { font-size: ${isPDF ? config.h2Size : "0.6rem"}; text-align: center; margin-bottom: 30px;}
     .receive {
@@ -62,11 +70,15 @@ const layoutStyles = (layout: string, isPDF: boolean, config: PdfSettings): stri
          margin:0px;
           }
          .sub_paragraph{
-         margin-top:10px;
+       margin-block:10px;
+
+       
          }
           .paragraph_title{
-          margin:0px;
-          margin-top:10px;
+          // margin:0px;
+          // padding:0px;
+         margin-top:26px;
+           margin-bottom:0px;
           font-style: italic;
           text-decoration: underline;
           font-weight: bold;
